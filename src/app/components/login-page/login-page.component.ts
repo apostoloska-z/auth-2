2b87eeb4650f9b0815599ac69d91b157c462285c
Zoia Apostoloska <z.apostolska@togizu.com>
2023-09-19 12:27:46 +0300

    com 3

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -41,7 +41,7 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('hehehehey4444')
+    console.log('hehehehey4444 66667773433')
     this.token$.subscribe(data => {
       console.log(data)
       localStorage.setItem('token', data);
