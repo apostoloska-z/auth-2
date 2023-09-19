d4889f692932de294b58089993d666a2f9b57909
Zoia Apostoloska <z.apostolska@togizu.com>
2023-09-19 12:26:48 +0300

    commit

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -54,6 +54,7 @@ export class LoginPageComponent implements OnInit {
   }
 
   onSubmit() {
+    console.log('hello')
 
     if (this.loginForm.invalid) {
       console.log('The information you have entered is not valid');
